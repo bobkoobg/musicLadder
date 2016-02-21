7faf494872858eaa2e626ac34c254a08465db60a
bobkoobg <boyko.surlev@gmail.com>
2016-02-21 20:39:07 +0100

    initial commit

---
+++ JavaApplication12MusicLadder/src/controller/MusicLadderController.java
@@ -0,0 +1,23 @@
+package controller;
+
+/**
+ *
+ * @author root
+ */
+public class MusicLadderController
+{
+
+    /**
+     * @param args the command line arguments
+     * Testing purposes!
+     */
+    public static void main(String[] args)
+    {
+        new MusicLadderController().helloWorld();
+    }
+    
+    private void helloWorld() {
+        System.out.println("Hello World!");
+    }
+    
+}
