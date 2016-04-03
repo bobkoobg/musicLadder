3127219fd129c6ee125c1e899d667207e7562b0a
bobkoobg <boyko.surlev@gmail.com>
2016-04-03 21:23:04 +0200

    Implement (v0.12.8): Working basic version of music ladder web page. TODO : Search for a way to not waste too much resources. Errors : Predictions incorrect placement.Missing : Insert songs, clear databases. Maybe : Entity classes can contain names instead of songIds in duels, so that the amount of request Client > Server can be decreased with ~70-80%.. Way to choose Ladder before loading songs. Ladder interface. Design. and then push v1 ^^

--- JavaApplication12MusicLadder/src/pages/musicLadder/musicLadderIndex.js
+++ JavaApplication12MusicLadder/src/pages/musicLadder/musicLadderIndex.js
@@ -4,6 +4,40 @@ console.log('hi musicLadderIndex.js');
 
 var $slider;
 var $saveDuelButton;
+var amountOfDuelsToPlay = 5;
+var amountOfPlayedDuels = 15;
+
+function loadSlider() {
+    $('.sliderr').slider({
+        value: 5,
+        min: 1,
+        max: 10,
+        step: 1
+    })
+    .each(function () {
+
+        // Add labels to slider whose values 
+        // are specified by min, max
+
+        // Get the options for this slider (specified above)
+        var opt = $(this).data().uiSlider.options;
+
+        // Get the number of possible values
+        var vals = opt.max - opt.min;
+
+        // Position the labels
+        for (var i = 0; i <= vals; i++) {
+
+            // Create a new element and position it with percentages
+            var el = $("<label style=\"position: absolute;width: 20px;margin-top: 20px;margin-left: -10px;text-align: center;\">" + (i + opt.min) + "</label>").css("left", (i / vals * 100) + "%");
+
+            // Add the element inside #slider
+            $(".sliderr").append(el);
+
+        }
+
+    });
+}
 
 function evaluateRating(currentRating, formerRating) {
     if (currentRating > formerRating) {
@@ -19,7 +53,7 @@ function displayPlayedDuels(data) {
     var style = "border: 1px solid black;";
     var style2 = "border: 1px solid black;font-weight: 700;";
     var song1Points, song1Update, song2Points, song2Update, style3, style4;
-
+    $("#playedDuelsList tbody").empty();
     $.each(data, function (curDuelID, curDuel) {
         loadSong(curDuel.song1ID, function (song1) {
             loadSong(curDuel.song2ID, function (song2) {
@@ -87,35 +121,46 @@ function loadPredictions(song1Rating, song2Rating, callback) {
 }
 
 function displayDuelsToPlay(data) {
-    var source = $("#duels-to-play-row-template").html();
-    var template = Handlebars.compile(source);
-    var context, html;
+    var source, template, context, html;
 
     var style = "border: 1px solid black;";
     var style2 = "border: 1px solid black;font-weight: 700;";
 
-
+    $("#duelsToPlayList tbody").empty();
     $.each(data, function (curDuelID, curDuel) {
         loadSong(curDuel.song1ID, function (song1) {
             loadSong(curDuel.song2ID, function (song2) {
                 if (curDuelID === 0) {
-                    loadPredictions(curDuel.song1BeforeMatchRating, curDuel.song2BeforeMatchRating, function (predictions, status) {
-                        $("#competitor1Name").text(song1.name);
-                        $("#competitor2Name").text(song2.name);
-                        $("#competitor1Rating").text(parseFloat(Math.round(curDuel.song1BeforeMatchRating * 100) / 100).toFixed(2));
-                        $("#competitor2Rating").text(parseFloat(Math.round(curDuel.song2BeforeMatchRating * 100) / 100).toFixed(2));
-                        $("#prediction1Win").text(parseFloat(Math.round(predictions[0] * 100) / 100).toFixed(2));
-                        $("#prediction1Draw").text(parseFloat(Math.round(predictions[2] * 100) / 100).toFixed(2));
-                        $("#prediction1Loss").text(parseFloat(Math.round(predictions[4] * 100) / 100).toFixed(2));
-                        $("#prediction2Win").text(parseFloat(Math.round(predictions[5] * 100) / 100).toFixed(2));
-                        $("#prediction2Draw").text(parseFloat(Math.round(predictions[3] * 100) / 100).toFixed(2));
-                        $("#prediction2Loss").text(parseFloat(Math.round(predictions[1] * 100) / 100).toFixed(2));
+                    loadPredictions(curDuel.song1BeforeMatchRating, curDuel.song2BeforeMatchRating, function (predictions) {
+                        $(".duelTemplateWrapper").remove();
+                        source = $("#current-duel-template").html();
+                        template = Handlebars.compile( source );
+
+                        context = {
+                            duelId: curDuel.duelID,
+                            song1Name: song1.name,
+                            song1Rating: curDuel.song1BeforeMatchRating,
+                            song2Name: song2.name,
+                            song2Rating: curDuel.song2BeforeMatchRating,
+                            predSong1Win: parseFloat(Math.round(predictions[0] * 100) / 100).toFixed(2),
+                            predSong1Draw: parseFloat(Math.round(predictions[2] * 100) / 100).toFixed(2),
+                            predSong1Loss: parseFloat(Math.round(predictions[4] * 100) / 100).toFixed(2),
+                            predSong2Loss: parseFloat(Math.round(predictions[1] * 100) / 100).toFixed(2),
+                            predSong2Draw: parseFloat(Math.round(predictions[3] * 100) / 100).toFixed(2),
+                            predSong2Win: parseFloat(Math.round(predictions[5] * 100) / 100).toFixed(2)
+                        };
+                        html = template(context);
+                        //$("#duelsToPlayList tbody").appendTo( html );
+                        $("#saveDuel").before( html );
+                        loadSlider();
                     });
                 }
-
+                source = $("#duels-to-play-row-template").html();
+                template = Handlebars.compile(source);
+                
                 context = {
-                    style: style,
                     duelID: curDuel.duelID,
+                    style: style,
                     song1Name: song1.name,
                     style2: style2,
                     song2Name: song2.name
@@ -126,6 +171,7 @@ function displayDuelsToPlay(data) {
             });
         });
     });
+    loadPlayedDuels( amountOfPlayedDuels );
 }
 
 function displaySongs(data) {
@@ -188,58 +234,67 @@ function loadPlayedDuels(amount) {
     });
 }
 
+function evaluateDuelGeneration( data ) {
+    console.log('data is : ' + JSON.stringify(data) );
+    loadDuelsToPlay( amountOfDuelsToPlay )
+    
+    
+}
+
+function generateDuels(amount) {
+    
+    $.ajax({
+        "url": "/musicLadderAPI/duel/1/" + amount,
+        "type": "PUT",
+        "headers": {"Content-Type": "application/json"},
+        "data": JSON.stringify({}),
+        "success": evaluateDuelGeneration
+    });
+}
+
+function evaluateDuelSave( data ) {
+    console.log('data is : ' + JSON.stringify(data) );
+    
+    $('#duelsToPlayListBody').filter(function(){
+        return $(this).data('duelid') === $('.row-duelTemplateWrapper').data("duelid")
+    }).remove();
+    
+    var curAmountOfDuels = 0;
+    $('.duelToPlay').each(function(i, obj) {
+        if ( $(obj).data('duelid') === $('.duelTemplateWrapper').data("duelid") ) {
+            $(obj).remove();
+        } else {
+            curAmountOfDuels++;
+        }
+    });
+    
+    if ( curAmountOfDuels < amountOfDuelsToPlay ) {
+        generateDuels( ( amountOfDuelsToPlay - curAmountOfDuels ) );
+    }
+}
+
 function saveDuel() {
-    console.log('saving...', $slider.slider("value"));
-//    $.ajax({
-//        "url": "/musicLadderAPI/duel",
-//        "type": "POST",
-//        "headers": {"Content-Type": "application/json"},
-//        "data": 
-//            JSON.stringify({ 'song1BeforeMatchRating': song1Rating,'song2BeforeMatchRating': song2Rating })
-//        ,
-//        "success": callback
-//    });
+    var duelId = $(".duelTemplateWrapper").data("duelid");
+    var sliderScore = $(".sliderr").slider("value");
+    
+    $.ajax({
+        "url": "/musicLadderAPI/duel/" + duelId + "/" + Math.abs(0 - sliderScore) + "/" + Math.abs(sliderScore - 10 ),
+        "type": "POST",
+        "headers": {"Content-Type": "application/json"},
+        "data": JSON.stringify({}),
+        "success": evaluateDuelSave
+    });
 }
 
 function load() {
 
     loadSongs();
-    loadDuelsToPlay(5);
-    loadPlayedDuels(15);
+    loadDuelsToPlay( amountOfDuelsToPlay );
 
     $saveDuelButton = $("#saveDuel");
     $slider = $(".sliderr");
 
     $saveDuelButton.on("click", saveDuel);
-    $slider.slider({
-        value: 5,
-        min: 1,
-        max: 10,
-        step: 1
-    })
-            .each(function () {
-
-                // Add labels to slider whose values 
-                // are specified by min, max
-
-                // Get the options for this slider (specified above)
-                var opt = $(this).data().uiSlider.options;
-
-                // Get the number of possible values
-                var vals = opt.max - opt.min;
-
-                // Position the labels
-                for (var i = 0; i <= vals; i++) {
-
-                    // Create a new element and position it with percentages
-                    var el = $("<label style=\"position: absolute;width: 20px;margin-top: 20px;margin-left: -10px;text-align: center;\">" + (i + opt.min) + "</label>").css("left", (i / vals * 100) + "%");
-
-                    // Add the element inside #slider
-                    $(".sliderr").append(el);
-
-                }
-
-            });
 }
 
 $(window).ready(load);
