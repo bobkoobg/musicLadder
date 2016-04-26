d58f361972e544ee3def034aa40ba10247f86e65
bobkoobg <boyko.surlev@gmail.com>
2016-04-26 11:22:59 +0200

    Implement (v0.16.6): DatabaseConnector Testing + Close connection moved to DatabaseConnector class for testing purposes. Added the functionality to manually change jdbc drive (in case of database change in future)

--- JavaApplication12MusicLadder/src/utils/DatabaseConnector.java
+++ JavaApplication12MusicLadder/src/utils/DatabaseConnector.java
@@ -3,6 +3,7 @@ package utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 /*
  * TODO before running this code :
@@ -11,57 +12,87 @@ import java.sql.SQLException;
  * Example code - http://www.mkyong.com/jdbc/connect-to-oracle-db-via-jdbc-driver-java/
  * Local host solution - http://stackoverflow.com/questions/18192521/ora-12505-tnslistener-does-not-currently-know-of-sid-given-in-connect-descript
  * Add ojdbc.jar to Libraries of Project : Right click -> Properties -> Libraries -> Add JAR
-*/
-public class DatabaseConnector
-{
+ */
+public class DatabaseConnector {
+
     private String databaseHost;
     private String databaseUsername;
     private String databasePassword;
+    private String jdbcDriverName = "oracle.jdbc.driver.OracleDriver";
 
-    public DatabaseConnector(String databaseHost, String databaseUsername, String databasePassword)
-    {
+    public DatabaseConnector( String databaseHost, String databaseUsername, String databasePassword, String jdbcDriverName ) {
         this.databaseHost = databaseHost;
         this.databaseUsername = databaseUsername;
         this.databasePassword = databasePassword;
+        if ( jdbcDriverName != null ) {
+            this.jdbcDriverName = jdbcDriverName;
+        }
     }
 
-    
-    public Boolean testJDBCdriver() {
-        //System.out.println("-------- Oracle JDBC Driver Testing --------");
-
+    public Boolean testJDBCdriver( Logger logger ) {
         try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-
-        } catch (ClassNotFoundException e) {
-
-            //System.out.println("Where is your Oracle JDBC Driver?");
-            e.printStackTrace();
+            Class.forName( jdbcDriverName );
+        } catch ( ClassNotFoundException e ) {
+            if ( logger != null ) {
+                logger.severe( "SQL Exception while trying to test the JDBC Driver " + e );
+            } else {
+                System.out.println( "SQL Exception while trying to test the JDBC Driver " + e );
+            }
             return false;
-
         }
-
-        //System.out.println("Oracle JDBC Driver Registered!");
         return true;
     }
-    
-    public Connection getConnection() {
-        //System.out.println("-------- Oracle JDBC Connection Testing --------");
+
+    public Connection getConnection( Logger logger ) {
         Connection connection = null;
+        if ( testJDBCdriver( logger ) ) {
+            try {
+                connection = DriverManager.getConnection( databaseHost, databaseUsername, databasePassword );
 
-        try {
-            connection = DriverManager.getConnection( databaseHost , databaseUsername , databasePassword );
+            } catch ( SQLException e ) {
+                if ( logger != null ) {
+                    logger.severe( "SQL Exception while trying to connect the connection to db " + e );
+                } else {
+                    System.out.println( "SQL Exception while trying to connect the connection to db " + e );
+                }
+                return null;
+            }
+            if ( logger != null ) {
+                logger.info( "Connection with database established successfully!" );
+            } else {
+                System.out.println( "Connection with database established successfully!" );
+            }
+            return connection;
+        }
+        return null;
+    }
 
-        } catch (SQLException e) {
-            //System.out.println("Connection Failed! Check output console");
-            e.printStackTrace();
-            return null;
+    public boolean closeConnection( Connection connection, Logger logger ) {
+        if ( connection != null ) {
+
+            try {
+                connection.close();
+            } catch ( SQLException e ) {
+                if ( logger != null ) {
+                    logger.severe( "SQL Exception while trying to close the connection to db " + e );
+                } else {
+                    System.out.println( "SQL Exception while trying to close the connection to db " + e );
+                }
+                return false;
+            }
+            if ( logger != null ) {
+                logger.info( "Connection with database closed successfully!" );
+            } else {
+                System.out.println( "Connection with database closed successfully!" );
+            }
+            return true;
         }
 
-//        if (connection != null) {
-//                System.out.println("You made it, take control of your database now!");
-//        } else {
-//                System.out.println("Failed to make connection!");
-//        }
-        return connection;
+        if ( logger != null ) {
+            logger.info( "Connection not initialized, cannot close!" );
+        } else {
+            System.out.println( "Connection not initialized, cannot close!" );
+        }
+        return false;
     }
 }
